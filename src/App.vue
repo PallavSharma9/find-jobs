69351ba951000a8063ef23ba588ee52ea5253d68
PallavSharma9 <pallavsharma0202@gmail.com>
2025-12-31 23:00:18 +0530

    Adding task manager

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const name = ref("Pallav Sharma");
+const status = ref("active");
+const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ name }}</h1>
+  <p>User is {{ status }}</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="index">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+  <br />
+
+  <button @click="toggleStatus">Change Status</button>
 </template>
 
 <style scoped></style>
