383df6d2d09d14f2b07c844535d52fce902a4b1c
PallavSharma9 <pallavsharma0202@gmail.com>
2026-01-02 09:48:20 +0530

    Adding tailwindcss

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import logo from "@/assets/img/logo.png";
+</script>
+
+<template>
+  <nav>
+    <div>
+      <div>
+        <div>
+          <!-- Logo -->
+          <h1 class="text-2xl font-bold underline text-blue-600">
+            Hello Tailwind
+          </h1>
+          <!-- /Logo  -->
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
