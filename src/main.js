34aaa5621a4d423176a7a891b56113ec65798a89
PallavSharma9 <pallavsharma0202@gmail.com>
2026-01-04 20:44:16 +0530

    updating the app adding routes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import "@/assets/main.css";
 import App from "./App.vue";
 import "primeicons/primeicons.css";
+import router from "./router";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
